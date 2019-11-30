5b79331599e0ffbeeda43f509fe738709b71e74b
ogamita777 <ogamita777@gmail.com>
2019-11-30 23:45:56 +0900

    module化

---
+++ src/board.js
@@ -0,0 +1,41 @@
+export default class {
+  constructor() {
+    this.m_column = 9;
+    this.m_row = 9;
+    this.m_cellSize = 64;
+  }
+
+  draw() {
+    const canvas = document.getElementById('board');
+
+    if(!canvas || !canvas.getContext) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, this.m_column * this.m_cellSize, this.m_row * this.m_cellSize);
+    ctx.strokeStyle = 'black';
+
+    for(let i = 0; i < this.m_column + 1; i++) {
+        const fromX = i * this.m_cellSize;
+        const fromY = 0;
+        const toX = fromX;
+        const toY = this.m_cellSize * this.m_row;
+
+        ctx.moveTo(fromX, fromY);
+        ctx.lineTo(toX, toY);
+        ctx.stroke();
+    }
+
+    for(let i = 0; i < this.m_row + 1; i++) {
+        const fromX = 0;
+        const fromY = i * this.m_cellSize;
+        const toX = this.m_column * this.m_cellSize;
+        const toY = fromY;
+
+        ctx.moveTo(fromX, fromY);
+        ctx.lineTo(toX, toY);
+        ctx.stroke();
+    }
+  }
+}
